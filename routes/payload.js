e891033c38914100a50df22a3305a04ae5416825
Ellmen <isaac.ellmen@thescore.com>
2017-10-28 19:24:59 -0400

    setup handler to clone repo

--- routes/payload.js
+++ routes/payload.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 var Git = require("nodegit");
@@ -15,9 +16,38 @@ router.post('/setup', function(req, res, next) {
     res.send('repo must be a github repo (https://github.com/*)');
     return;
   }
-  Git.Clone(`${repo}`, "./repos")
-  .then(console.log('lol'));
-  res.send(req.body.blah);
+  var { repo, name } = req.body;
+  fs.readFile('./repoIndex.json', 'utf8', function readFileCallback(err, data) {
+    console.log(repo);
+    if (err) {
+      console.log(err);
+      res.send('Something went wrong :/');
+      return;
+    }
+    var repoIndex = JSON.parse(data); //now it an object
+    if (repoIndex[repo]) {
+      res.send('Already set up');
+    } else {
+      Git.Clone(`${repo}`, "./repos")
+      .then(function () {
+        repoIndex[repo] = name;
+        var json = JSON.stringify(repoIndex);
+        fs.writeFile('./repoIndex.json', json, 'utf8', function writeFileCallback(err, data) {
+          if (err) {
+            console.log(err);
+            res.send('Could not write to store');
+            return;
+          } else {
+            res.send('All set up!');
+          }
+        });
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.send('Could not clone the repo');
+      });
+    }
+  });
 });
 
 module.exports = router;
